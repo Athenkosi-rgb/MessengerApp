e70ec54e77a13cbcaa31c513e90d20d250130070
Athenkosi-rgb <82506682+Athenkosi-rgb@users.noreply.github.com>
2021-10-10 15:23:39 +0200

    commit man

--- js/main.js
+++ js/main.js
@@ -18,15 +18,14 @@ function switchChannel(channel) /*channel we want to highlight.*/ {
   console.log(`"selectedChannel" now equals to "${selectedChannel.name}"`);
 }
 
-// Therefore we need to add an id “channelName” to the <h1> tag in the “message-area”.
+function showHeader() {
+  document.getElementById("channelName").innerHTML = selectedChannel.name;
+}
 
-// Now we can start writing a function we call “showHeader()”. We do not need to pass any arguments to it.
-
-// We have two elements we want to address with the function, the channel name and the favorite button. As we want to change the actual html code with our function we need to address the innerHTML of the specific element. In order to do so document.getElementById('channelName').innerHTML = ... is the syntax we need.
-
-// Tip: Remember that the selectedChannel variable always contains the newest selected channel. Then you only need to insert the respective name of the channel object to the syntax you just saw.
-
-// We need to do the same thing but with the favorite button. Here it gets tricky because we need to use our ternary operator ? to distinguish if the favorite attribute of the object is “true” or “false”. If it is true we want to use the “favorite” icon. If it is false we want to use the “favorite_border” icon.
+// We need to do the same thing but with the favorite button.
+// Here it gets tricky because we need to use our ternary operator ?
+// to distinguish if the favorite attribute of the object is “true” or “false”.
+// If it is true we want to use the “favorite” icon. If it is false we want to use the “favorite_border” icon.
 // Tip: If you are struggeling try to google for examples of how to use the ternary operator.
 
 // The last thing we need to do is to call the “showHeader()” function within our “switchChannel(channel)” function.
