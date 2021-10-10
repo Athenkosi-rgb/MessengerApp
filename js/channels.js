889e5142f8514158ef5322c536bc2a387460b0ab
Athenkosi-rgb <82506682+Athenkosi-rgb@users.noreply.github.com>
2021-10-10 22:23:16 +0200

    Week 5 Challenge: Attempt 1

--- js/channels.js
+++ js/channels.js
@@ -1,20 +1,54 @@
+// const channel1 = {
+//   name: "MeetUp", //name of channel
+//   id: "channel1", //name of object
+//   favorite: true, // TRUE when the channel = “Favorites” section, FALSE = “Regular” section.
+// };
+// const channel2 = {
+//   name: "OktoberFest",
+//   id: "channel2",
+//   favorite: true,
+// };
+// const channel3 = {
+//   name: "WeatherChannel",
+//   id: "channel3",
+//   favorite: false,
+// };
+// const channel4 = {
+//   name: "SevenContinents",
+//   id: "channel4",
+//   favorite: false,
+// };
+
+//=====================================================================================//
+// Improved channels
+
 const channel1 = {
-  name: "MeetUp", //name of channel
-  id: "channel1", //name of object
-  favorite: true, // TRUE when the channel = “Favorites” section, FALSE = “Regular” section.
+  id: "channel1",
+  name: "MeetUp",
+  favorite: true,
+  messages: [],
+  latestMessage: "17:24",
 };
 const channel2 = {
-  name: "OktoberFest",
   id: "channel2",
+  name: "OktoberFest",
   favorite: true,
+  messages: [],
+  latestMessage: "17:24",
 };
 const channel3 = {
-  name: "WeatherChannel",
   id: "channel3",
+  name: "WeatherChannel",
   favorite: false,
+  messages: [],
+  latestMessage: "17:24",
 };
 const channel4 = {
-  name: "SevenContinents",
   id: "channel4",
+  name: "SevenContinents",
   favorite: false,
+  messages: [],
+  latestMessage: "17:24",
 };
+
+const mockChannels = [channel1, channel2, channel3, channel4];
---
+++ js/messages.js
@@ -0,0 +1,30 @@
+const message1 = {
+  createdBy: "Jack Blue",
+  createdOn: new Date("December 08, 2018 17:24:00"),
+  channel: "OktoberFest",
+  own: false,
+  text: "Yo, What's up?",
+};
+const message2 = {
+  createdBy: "Jack Blue",
+  createdOn: new Date("December 08, 2018 17:24:00"),
+  channel: "OktoberFest",
+  own: false,
+  text: "Yo, What's up?",
+};
+const message3 = {
+  createdBy: "Jack Blue",
+  createdOn: new Date("December 08, 2018 17:24:00"),
+  channel: "OktoberFest",
+  own: false,
+  text: "Yo, What's up?",
+};
+const message4 = {
+  createdBy: "Jack Blue",
+  createdOn: new Date("December 08, 2018 17:24:00"),
+  channel: "OktoberFest",
+  own: false,
+  text: "Yo, What's up?",
+};
+
+const mockMessages = [message1, message2, message3, message4];
